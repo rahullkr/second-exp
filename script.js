135846054743f57df6e4dae79716d33d26d77c0f
Rahul Kumar <79074470+rahullkr@users.noreply.github.com>
2023-04-16 18:55:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshLambertMaterial({ color: 0xb3b5b5 });
+var material = new THREE.MeshLambertMaterial({ color: 0x98e632});
 
 // var mesh = new THREE.Mesh(geometry, material);
 // mesh.position.y = 2;
